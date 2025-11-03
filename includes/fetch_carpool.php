483b2a60b2619723144f6f77789b9358cf128f7d
Noelle Adame <2241743@slu.edu.ph>
2025-11-04 01:57:53 +0800

    Add search and filter on list of carpools

--- includes/fetch_carpool.php
+++ includes/fetch_carpool.php
@@ -26,6 +26,50 @@ $pipeline = [
     ]
 ];
 
+$match = [];
+
+if (!empty($_GET['search'])) {
+    $search = $_GET['search'];
+    $match['$or'] = [
+        ['driverInfo.name' => ['$regex' => $search, '$options' => 'i']],
+        ['stationedAt' => ['$regex' => $search, '$options' => 'i']],
+        ['destination' => ['$regex' => $search, '$options' => 'i']],
+    ];
+}
+
+if (!empty($_GET['for'])) {
+    $match['for'] = $_GET['for'];
+}
+
+if (!empty($_GET['seat'])) {
+    $seat = (int) $_GET['seat'];
+    if ($seat >= 4) {
+        $match['availableSeats'] = ['$gte' => 4];
+    } else {
+        $match['availableSeats'] = $seat;
+    }
+}
+
+if (!empty($_GET['booked'])) {
+    if ($_GET['booked'] === 'empty') {
+        $match['bookedSeats'] = 0;
+    } elseif ($_GET['booked'] === 'partial') {
+        $match['$expr'] = [
+            '$and' => [
+                ['$gt' => ['$bookedSeats', 0]],
+                ['$lt' => ['$bookedSeats', '$availableSeats']]
+            ]
+        ];
+    } elseif ($_GET['booked'] === 'full') {
+        $match['$expr'] = [
+            '$eq' => ['$bookedSeats', '$availableSeats']
+        ];
+    }
+}
+
+if (!empty($match)) {
+    $pipeline[] = ['$match' => $match];
+}
 // Execute aggregation pipeline
 $results = $ridesCollection->aggregate($pipeline);
 
